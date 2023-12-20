df2f653fc9eea3f7f52696ba3fb54a7c8a734bba
DarkShadow390 <angeokou39@gmail.com>
2023-12-21 00:55:28 +0100

    env test

---
+++ appTwl/App/Manager/bdd/env_test.php
@@ -0,0 +1,10 @@
+<?php
+    $host= '';
+    $database = '';
+    $login= '';
+    $password = '';
+    $serveur_messagerie ="";
+    $login_messagerie ="";
+    $password_messagerie ="";
+    $port_messagerie ="";
+?>
